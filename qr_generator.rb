985d4f841a1d9c70c5e5545dd866d9149aef3d71
Jordan Hudgens <jordan@devcamp.com>
2021-04-14 11:37:51 -0700

    Added a qr generator script

---
+++ qr_generator.rb
@@ -0,0 +1,15 @@
+require 'rqrcode'
+
+url = ARGV[0]
+color = ARGV[1] ? "##{ARGV[1]}" : 'black'
+
+qr_code = RQRCode::QRCode.new(url)
+
+png = qr_code.as_png(
+  color: color,
+  fill: "white",
+  size: 300
+)
+
+IO.binwrite("website.png", png.to_s)
+
